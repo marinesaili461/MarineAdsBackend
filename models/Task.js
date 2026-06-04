ec5f91a0d7b5d88b9b872fb0b71c6b9070e4c3c5
marinehaley3 <marinehaley3@gmail.com>
2026-06-04 21:55:01 +0300

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -1,41 +1,16 @@
-// models/Task.js
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema(
   {
-    title: {
-      type: String,
-      required: [true, "Task title is required"],
-    },
-    description: {
-      type: String,
-    },
-    category: {
-      type: String,
-      enum: ["survey", "video", "signup", "social", "offer", "watch", "other"],
-      default: "other",
-    },
-    reward: {
-      type: Number,
-      required: true,
-    },
-    link: {
-      type: String,
-    },
-    createdBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    isActive: {
-      type: Boolean,
-      default: true,
-    },
+    title: { type: String, required: [true, "Task title is required"] },
+    description: { type: String },
+    category: { type: String, enum: ["survey", "video", "signup", "social", "offer", "watch", "other"], default: "other" },
+    reward: { type: Number, required: true },
+    link: { type: String },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    isActive: { type: Boolean, default: true },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-const Task = mongoose.model("Task", taskSchema);
-module.exports = Task;
+export default mongoose.model("Task", taskSchema);
