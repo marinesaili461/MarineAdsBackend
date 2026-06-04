d94ff7458f7a9bb73355a6c5437b9167cd88ee9c
marinehaley3 <marinehaley3@gmail.com>
2026-06-04 21:55:45 +0300

    Update Token.js

--- models/Token.js
+++ models/Token.js
@@ -1,24 +1,10 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const tokenSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
-  },
-  token: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    enum: ["verify", "reset"],
-    required: true,
-  },
-  expiresAt: {
-    type: Date,
-    required: true,
-  },
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+  token: { type: String, required: true },
+  type: { type: String, enum: ["verify", "reset"], required: true },
+  expiresAt: { type: Date, required: true },
 });
 
-module.exports = mongoose.model("Token", tokenSchema);
+export default mongoose.model("Token", tokenSchema);
