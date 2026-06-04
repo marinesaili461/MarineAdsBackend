bbcb7dda99b2211a4598f15a6191c95786afa4b6
marinehaley3 <marinehaley3@gmail.com>
2026-06-04 21:52:55 +0300

    Update ChatRoom.js

--- models/ChatRoom.js
+++ models/ChatRoom.js
@@ -1,9 +1,12 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const chatRoomSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  isGroup: { type: Boolean, default: false },
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-}, { timestamps: true });
+const chatRoomSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    isGroup: { type: Boolean, default: false },
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("ChatRoom", chatRoomSchema);
+export default mongoose.model("ChatRoom", chatRoomSchema);
