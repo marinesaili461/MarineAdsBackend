75bab8ab85402f54bba45c61365b793ceb95fd16
marinehaley3 <marinehaley3@gmail.com>
2026-06-04 21:53:22 +0300

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,29 +1,12 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
   {
-    chatRoom: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "ChatRoom",
-      required: true,
-    },
-    sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    content: {
-      type: String,
-      required: true,
-    },
-    type: {
-      type: String,
-      default: "text",
-    },
-    readStatus: {
-      type: Boolean,
-      default: false,
-    },
+    chatRoom: { type: mongoose.Schema.Types.ObjectId, ref: "ChatRoom", required: true },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: true },
+    type: { type: String, default: "text" },
+    readStatus: { type: Boolean, default: false },
     reactions: [
       {
         user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
@@ -34,4 +17,4 @@ const messageSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Message", messageSchema);
+export default mongoose.model("Message", messageSchema);
