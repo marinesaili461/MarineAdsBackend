5b1021c672b14b3d934ce4183d640169d44c5443
marinehaley3 <marinehaley3@gmail.com>
2026-06-06 18:47:13 +0300

    Update RewardCodeRoutes.js

--- routes/RewardCodeRoutes.js
+++ routes/RewardCodeRoutes.js
@@ -1,13 +1,23 @@
 import { Router } from "express";
-import { createRewardCode, deactivateRewardCode, listRewardCodes, redeemRewardCode } from "../Controllers/RewardCodeController.js";
+import {
+  createRewardCode,
+  deactivateRewardCode,
+  deleteRewardCode,
+  listRewardCodes,
+  redeemRewardCode,
+} from "../Controllers/RewardCodeController.js";
 import { protect } from "../Middlewares/authMiddleware.js";
 import restrictTo from "../Middlewares/roleMiddleware.js";
 
 const router = Router();
 
-router.post("/create", protect, restrictTo("admin"), createRewardCode);
-router.patch("/deactivate/:codeId", protect, restrictTo("admin"), deactivateRewardCode);
-router.get("/list", protect, restrictTo("admin"), listRewardCodes);
+// Admin routes
+router.post("/create", protect, restrictTo("admin", "superadmin"), createRewardCode);
+router.get("/list", protect, restrictTo("admin", "superadmin"), listRewardCodes);
+router.patch("/deactivate/:codeId", protect, restrictTo("admin", "superadmin"), deactivateRewardCode);
+router.delete("/delete/:codeId", protect, restrictTo("admin", "superadmin"), deleteRewardCode);
+
+// User route
 router.post("/redeem", protect, redeemRewardCode);
 
 export default router;
