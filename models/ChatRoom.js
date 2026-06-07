bf1ed1bb1ffebf828d1d16f9784173ba3254e881
marinehaley3 <marinehaley3@gmail.com>
2026-06-07 23:06:10 +0300

    Update ChatRoom.js

--- models/ChatRoom.js
+++ models/ChatRoom.js
@@ -1,10 +1,22 @@
 import mongoose from "mongoose";
 
+const chatBanSchema = new mongoose.Schema({
+  user:      { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  bannedBy:  { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  bannedAt:  { type: Date, default: Date.now },
+  expiresAt: { type: Date, required: true }, // computed from hours or days
+  reason:    { type: String },
+});
+
 const chatRoomSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    isGroup: { type: Boolean, default: false },
-    members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    name:     { type: String, required: true },
+    isGroup:  { type: Boolean, default: false },
+    members:  [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    isClosed: { type: Boolean, default: false },
+    closedAt: { type: Date },
+    closedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    bans:     [chatBanSchema],
   },
   { timestamps: true }
 );
