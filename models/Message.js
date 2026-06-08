1f1943592fdf9164a1c38653206224863a9a03ee
marinehaley3 <marinehaley3@gmail.com>
2026-06-08 13:51:17 +0300

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -2,14 +2,17 @@ import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
   {
-    chatRoom: { type: mongoose.Schema.Types.ObjectId, ref: "ChatRoom", required: true },
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    content: { type: String, required: true },
-    type: { type: String, default: "text" },
-    readStatus: { type: Boolean, default: false },
+    chatRoom:       { type: mongoose.Schema.Types.ObjectId, ref: "ChatRoom", required: true },
+    sender:         { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    content:        { type: String, required: true },
+    type:           { type: String, default: "text" },
+    imageUrl:       { type: String, default: null },
+    isAdminMessage: { type: Boolean, default: false },
+    readStatus:     { type: Boolean, default: false },
+    replyTo:        { type: mongoose.Schema.Types.ObjectId, ref: "Message", default: null },
     reactions: [
       {
-        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        user:  { type: mongoose.Schema.Types.ObjectId, ref: "User" },
         emoji: String,
       },
     ],
