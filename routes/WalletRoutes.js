5d9ae326f402376c294cf81cad65ecb8e9f1396f
marinesaili461 <marinesaili461@gmail.com>
2026-06-17 18:23:45 +0300

    Update WalletRoutes.js

--- routes/WalletRoutes.js
+++ routes/WalletRoutes.js
@@ -1,13 +1,20 @@
 import { Router } from "express";
 import { protect } from "../Middlewares/authMiddleware.js";
-import { getWallet, deposit, withdraw, getTransactions, getWithdrawalHistory } from "../Controllers/WalletController.js";
+import { isAdmin } from "../Middlewares/adminMiddleware.js";
+import {
+  getWallet, deposit, getTransactions,
+  getAllDeposits, approveDeposit, rejectDeposit,
+} from "../Controllers/WalletController.js";
 
 const router = Router();
 
 router.get("/", protect, getWallet);
 router.post("/deposit", protect, deposit);
-router.post("/withdraw", protect, withdraw);
 router.get("/transactions", protect, getTransactions);
-router.get("/withdrawals", protect, getWithdrawalHistory);
+
+// admin
+router.get("/admin/deposits",        protect, isAdmin, getAllDeposits);
+router.put("/admin/deposits/:id/approve", protect, isAdmin, approveDeposit);
+router.put("/admin/deposits/:id/reject",  protect, isAdmin, rejectDeposit);
 
 export default router;
